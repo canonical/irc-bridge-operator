bbdebf02150c13da8ce383f8dbe18bbab14c3d6f
Mariyan Dimitrov <mariyan.dimitrov@canonical.com>
2024-09-03 16:06:47 +0300

    tests(irc): More irc tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -18,11 +18,11 @@ logger = logging.getLogger(__name__)
 
 @pytest.mark.asyncio
 @pytest.mark.abort_on_fail
-async def test_lifecycle(app: ops.model.Application, ops_test: OpsTest):
+async def test_lifecycle_before_relations(app: ops.model.Application, ops_test: OpsTest):
     """
     arrange: build and deploy the charm.
     act: nothing.
-    assert: that the charm ends up in an active state.
+    assert: that the charm ends up in blocked state because of missing relations.
     """
     # Set config so the charm can start
     config = {"bridge_admins": "admin:example.com", "bot_nickname": "bot"}
@@ -31,20 +31,4 @@ async def test_lifecycle(app: ops.model.Application, ops_test: OpsTest):
     unit = app.units[0]  # type: ignore
 
     # Mypy has difficulty with ActiveStatus
-    assert unit.workload_status == ops.model.ActiveStatus.name  # type: ignore
-
-    await tests.integration.helpers.dispatch_to_unit(ops_test, unit, "stop")
-    time.sleep(5)
-    _, service_status, _ = await ops_test.juju(
-        "exec", "--unit", unit.name, "snap services matrix-appservice-irc"
-    )
-    logger.info(service_status)
-    assert "inactive" in service_status
-
-    await tests.integration.helpers.dispatch_to_unit(ops_test, unit, "start")
-    time.sleep(5)
-    _, service_status, _ = await ops_test.juju(
-        "exec", "--unit", unit.name, "snap services matrix-appservice-irc"
-    )
-    logger.info(service_status)
-    assert "active" in service_status
+    assert unit.workload_status == ops.model.BlockedStatus.name  # type: ignore

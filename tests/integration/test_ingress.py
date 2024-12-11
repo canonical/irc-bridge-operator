1dce9f37abda39411aa01f1048fa264987afe357
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2024-12-11 13:58:29 -0300

    create new fixture for irc integrated and set to work with ingress

--- tests/integration/test_ingress.py
+++ tests/integration/test_ingress.py
@@ -20,19 +20,14 @@ logger = logging.getLogger(__name__)
 
 @pytest.mark.asyncio
 @pytest.mark.abort_on_fail
-async def test_ingress_integration(app: Application, model: Model):
+async def test_ingress_integration(app_integrated: Application, model: Model):
     """
     arrange: assert IRC /health is ok, deploy haproxy and relate it to
         self-signed-certificates. Relate haproxy with IRC bridge.
     act: request /health via haproxy.
     assert: request is successful.
     """
-    # Deploy postgresql charm and relate with the charm
-    await model.deploy("postgresql", channel="14/stable")
-    await model.wait_for_idle(apps=["postgresql"], status="active", timeout=60 * 60)
-    await model.add_relation(app.name, "postgresql")
-    await model.wait_for_idle(apps=[f"{app.name}", "postgresql"], status="active", timeout=60 * 60)
-    unit_address = await tests.integration.helpers.get_unit_address(app)
+    unit_address = await tests.integration.helpers.get_unit_address(app_integrated)
     response = requests.get(f"http://{unit_address}:8090/health", timeout=30)
     assert response.status_code == 200
     assert response.text == "OK"
@@ -47,8 +42,10 @@ async def test_ingress_integration(app: Application, model: Model):
     await model.wait_for_idle(
         apps=[self_signed_application.name, haproxy_application.name], status="active"
     )
-    await model.add_relation(app.name, haproxy_application.name)
-    await model.wait_for_idle(apps=[app.name, haproxy_application.name], status="active")
+    await model.add_relation(app_integrated.name, haproxy_application.name)
+    await model.wait_for_idle(
+        apps=[app_integrated.name, haproxy_application.name], status="active"
+    )
 
     unit_address = await tests.integration.helpers.get_unit_address(haproxy_application)
     session = Session()

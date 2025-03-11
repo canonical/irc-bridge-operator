64d1aacd99998c56b8e45fbf05f9712189290252
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2025-03-11 14:35:59 -0300

    Put white line back

--- src/database_observer.py
+++ src/database_observer.py
@@ -61,6 +61,7 @@ class DatabaseObserver(Object):
                 fields=["uris", "endpoints", "username", "password", "database"]
             ).values()
         )
+
         if not relation_data:
             return None
 
